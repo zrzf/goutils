22e9a319645735e9124375678781b611a987ba67
zrzf <79478101+zrzf@users.noreply.github.com>
2021-03-11 04:33:51 -0300

    Removed string functions

--- slices/slices.go
+++ slices/slices.go
@@ -4,19 +4,6 @@ import (
 	"math/rand"
 )
 
-func ChunkStr(list []string, chunksize int) [][]string {
-	lenlist := len(list)
-	var chunks [][]string
-	for i := 0; i < lenlist; i += chunksize {
-		end := i + chunksize
-		if end > lenlist {
-			end = lenlist
-		}
-		chunks = append(chunks, list[i:end])
-	}
-	return chunks
-}
-
 func ChunkInt(list []int, chunksize int) [][]int {
 	lenlist := len(list)
 	var chunks [][]int
@@ -28,15 +15,6 @@ func ChunkInt(list []int, chunksize int) [][]int {
 		chunks = append(chunks, list[i:end])
 	}
 	return chunks
-}
-
-func IndexStr(list []string, str string) int {
-	for i, v := range list {
-		if v == str {
-			return i
-		}
-	}
-	return -1
 }
 
 func IndexInt(list []int, num int) int {
@@ -51,15 +29,6 @@ func IndexInt(list []int, num int) int {
 func ContainInt(list []int, num int) bool {
 	for _, v := range list {
 		if v == num {
-			return true
-		}
-	}
-	return false
-}
-
-func ContainStr(list []string, str string) bool {
-	for _, v := range list {
-		if v == str {
 			return true
 		}
 	}
@@ -99,20 +68,6 @@ func SumInt(list []int) int {
 	return s
 }
 
-func UniqueStr(list *[]string) {
-	var c uint
-	check := make(map[string]uint)
-	for _, val := range *list {
-		check[val] = 1
-	}
-	res := make([]string,len(check))
-	for letter, _ := range check {
-		res[c] = letter
-		c++
-	}
-	*list = res
-}
-
 func UniqueInt(list *[]int) {
 	var c uint
 	check := make(map[int]uint)
@@ -139,19 +94,6 @@ func UniqueUint(list *[]uint) {
 		c++
 	}
 	*list = res
-}
-
-func CountStr(list []string, str ...string) int {
-	var c int
-	for _, val := range list {
-		for _, s := range str {
-			if val == s {
-				c++
-				break
-			}
-		}
-	}
-	return c
 }
 
 func CountInt(list []int, num ...int) int {
@@ -187,27 +129,6 @@ func FilterInt(list *[]int, filter ...int) {
 	*list = s
 }
 
-func FilterStr(list *[]string, filter ...string) {
-	var c int
-	var add bool
-	s := make([]string, len(*list) - CountStr(*list, filter...))
-	for _, val := range *list {
-		add = true
-		for _, f := range filter {
-			if val == f {
-				add = false
-				break
-			}
-		}
-		if add {
-			s[c] = val
-			c++
-		}
-	}
-	*list = s
-}
-
-
 func RangeInt(min int, max int) []int {
 	var i int
 	rangeslice := make([]int, max - min + 1)
@@ -234,22 +155,6 @@ func ShuffleInt(slice []int, seed int64) {
 func ShuffleUint(slice []uint, seed int64) {
 	rand.Seed(seed)
 	rand.Shuffle(len(slice), func(i, j int) { (slice)[i], (slice)[j] = (slice)[j], (slice)[i] })
-}
-
-func ShuffleStr(slice []string, seed int64) {
-	rand.Seed(seed)
-	rand.Shuffle(len(slice), func(i, j int) { (slice)[i], (slice)[j] = (slice)[j], (slice)[i] })
-}
-
-func ReverseStr(slice []string) []string {
-	var c int
-	slicelen := len(slice)
-	s := make([]string, slicelen)
-	for i:=slicelen-1;i>=0;i--{
-		s[c] = slice[i]
-		c++
-	}
-	return s
 }
 
 func ReverseInt(slice []int) []int {
