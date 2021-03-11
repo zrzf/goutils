2290e7a13c419be8810ca9260f5084010c046e9b
zrzf <79478101+zrzf@users.noreply.github.com>
2021-03-11 13:47:25 -0300

    Removed int functions

--- slices/slices.go
+++ slices/slices.go
@@ -4,37 +4,6 @@ import (
 	"math/rand"
 )
 
-func ChunkInt(list []int, chunksize int) [][]int {
-	lenlist := len(list)
-	var chunks [][]int
-	for i := 0; i < lenlist; i += chunksize {
-		end := i + chunksize
-		if end > lenlist {
-			end = lenlist
-		}
-		chunks = append(chunks, list[i:end])
-	}
-	return chunks
-}
-
-func IndexInt(list []int, num int) int {
-	for i, v := range list {
-		if v == num {
-			return i
-		}
-	}
-	return -1
-}
-
-func ContainInt(list []int, num int) bool {
-	for _, v := range list {
-		if v == num {
-			return true
-		}
-	}
-	return false
-}
-
 func SumFloat64(list []float64) float64 {
 	var s float64
 	for _, v := range list {
@@ -42,7 +11,6 @@ func SumFloat64(list []float64) float64 {
 	}
 	return s
 }
-
 
 func SumFloat32(list []float32) float32 {
 	var s float32
@@ -60,28 +28,6 @@ func SumUint(list []uint) uint {
 	return s
 }
 
-func SumInt(list []int) int {
-	var s int
-	for _, v := range list {
-		s = s + v
-	}
-	return s
-}
-
-func UniqueInt(list *[]int) {
-	var c uint
-	check := make(map[int]uint)
-	for _, val := range *list {
-		check[val] = 1
-	}
-	res := make([]int, len(check))
-	for num, _ := range check {
-		res[c] = num
-		c++
-	}
-	*list = res
-}
-
 func UniqueUint(list *[]uint) {
 	var c uint
 	check := make(map[uint]uint)
@@ -96,74 +42,16 @@ func UniqueUint(list *[]uint) {
 	*list = res
 }
 
-func CountInt(list []int, num ...int) int {
-	var c int
-	for _, val := range list {
-		for _, n := range num {
-			if val == n {
-				c++
-				break
-			}
-		}
-	}
-	return c
-}
-
-func FilterInt(list *[]int, filter ...int) {
-	var c int
-	var add bool
-	s := make([]int, len(*list) - CountInt(*list, filter...))
-	for _, val := range *list {
-		add = true
-		for _, f := range filter {
-			if val == f {
-				add = false
-				break
-			}
-		}
-		if add {
-			s[c] = val
-			c++
-		}
-	}
-	*list = s
-}
-
-func RangeInt(min int, max int) []int {
-	var i int
-	rangeslice := make([]int, max - min + 1)
-	for i=0;i<len(rangeslice);i++{
-		rangeslice[i] = min + i
-	}
-	return rangeslice
-}
-
 func RangeUint(min uint, max uint) []uint {
 	var i uint
-	rangeslice := make([]uint, max - min + 1)
-	for i=0;i<uint(len(rangeslice));i++{
+	rangeslice := make([]uint, max-min+1)
+	for i = 0; i < uint(len(rangeslice)); i++ {
 		rangeslice[i] = min + (i)
 	}
 	return rangeslice
 }
 
-func ShuffleInt(slice []int, seed int64) {
-	rand.Seed(seed)
-	rand.Shuffle(len(slice), func(i, j int) { (slice)[i], (slice)[j] = (slice)[j], (slice)[i] })
-}
-
 func ShuffleUint(slice []uint, seed int64) {
 	rand.Seed(seed)
 	rand.Shuffle(len(slice), func(i, j int) { (slice)[i], (slice)[j] = (slice)[j], (slice)[i] })
-}
-
-func ReverseInt(slice []int) []int {
-	var c int
-	slicelen := len(slice)
-	s := make([]int, slicelen)
-	for i:=slicelen-1;i>=0;i--{
-		s[c] = slice[i]
-		c++
-	}
-	return s
 }
