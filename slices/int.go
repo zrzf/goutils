12d573373fe2bbe2e287bce6d088ea732ffc2043
zrzf <79478101+zrzf@users.noreply.github.com>
2021-03-11 13:48:27 -0300

    Add int related slices to int.go

---
+++ slices/int.go
@@ -0,0 +1,116 @@
+package slices
+
+import (
+	"math/rand"
+)
+
+func ChunkInt(list []int, chunksize int) [][]int {
+	lenlist := len(list)
+	var chunks [][]int
+	for i := 0; i < lenlist; i += chunksize {
+		end := i + chunksize
+		if end > lenlist {
+			end = lenlist
+		}
+		chunks = append(chunks, list[i:end])
+	}
+	return chunks
+}
+
+func IndexInt(list []int, num int) int {
+	for i, v := range list {
+		if v == num {
+			return i
+		}
+	}
+	return -1
+}
+
+func ContainInt(list []int, num int) bool {
+	for _, v := range list {
+		if v == num {
+			return true
+		}
+	}
+	return false
+}
+
+func SumInt(list []int) int {
+	var s int
+	for _, v := range list {
+		s = s + v
+	}
+	return s
+}
+
+func UniqueInt(list *[]int) {
+	var c uint
+	check := make(map[int]uint)
+	for _, val := range *list {
+		check[val] = 1
+	}
+	res := make([]int, len(check))
+	for num, _ := range check {
+		res[c] = num
+		c++
+	}
+	*list = res
+}
+
+func CountInt(list []int, num ...int) int {
+	var c int
+	for _, val := range list {
+		for _, n := range num {
+			if val == n {
+				c++
+				break
+			}
+		}
+	}
+	return c
+}
+
+func FilterInt(list *[]int, filter ...int) {
+	var c int
+	var add bool
+	s := make([]int, len(*list)-CountInt(*list, filter...))
+	for _, val := range *list {
+		add = true
+		for _, f := range filter {
+			if val == f {
+				add = false
+				break
+			}
+		}
+		if add {
+			s[c] = val
+			c++
+		}
+	}
+	*list = s
+}
+
+func RangeInt(min int, max int) []int {
+	var i int
+	rangeslice := make([]int, max-min+1)
+	for i = 0; i < len(rangeslice); i++ {
+		rangeslice[i] = min + i
+	}
+	return rangeslice
+}
+
+func ShuffleInt(slice []int, seed int64) {
+	rand.Seed(seed)
+	rand.Shuffle(len(slice), func(i, j int) { (slice)[i], (slice)[j] = (slice)[j], (slice)[i] })
+}
+
+func ReverseInt(slice []int) []int {
+	var c int
+	slicelen := len(slice)
+	s := make([]int, slicelen)
+	for i := slicelen - 1; i >= 0; i-- {
+		s[c] = slice[i]
+		c++
+	}
+	return s
+}
