1aa820e0eb1562b385be44f3fb03b6c8484529cf
zrzf <79478101+zrzf@users.noreply.github.com>
2021-03-11 15:24:54 -0300

    Add uint related slices to uint.go

---
+++ slices/uint.go
@@ -0,0 +1,41 @@
+package slices
+
+import (
+	"math/rand"
+)
+
+func SumUint(list []uint) uint {
+	var s uint
+	for _, v := range list {
+		s = s + v
+	}
+	return s
+}
+
+func UniqueUint(list *[]uint) {
+	var c uint
+	check := make(map[uint]uint)
+	for _, val := range *list {
+		check[val] = 1
+	}
+	res := make([]uint, len(check))
+	for num, _ := range check {
+		res[c] = num
+		c++
+	}
+	*list = res
+}
+
+func RangeUint(min uint, max uint) []uint {
+	var i uint
+	rangeslice := make([]uint, max-min+1)
+	for i = 0; i < uint(len(rangeslice)); i++ {
+		rangeslice[i] = min + (i)
+	}
+	return rangeslice
+}
+
+func ShuffleUint(slice []uint, seed int64) {
+	rand.Seed(seed)
+	rand.Shuffle(len(slice), func(i, j int) { (slice)[i], (slice)[j] = (slice)[j], (slice)[i] })
+}
